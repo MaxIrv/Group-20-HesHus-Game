58e64c51879dd2eaf52822d095d1505c3dd1a6aa
SJButla <160864718+SJButla@users.noreply.github.com>
2024-05-15 17:55:41 +0100

    new town map

---
+++ assets/Town/Tilemaps/black_pixel_art.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pixil-frame-0" tilewidth="24" tileheight="24" tilecount="1" columns="1">
+ <image source="pixil-frame-0.png" width="24" height="24"/>
+</tileset>
---
+++ assets/Town/Tilemaps/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="house" tilewidth="24" tileheight="24" tilecount="28" columns="4">
+ <image source="../Textures/house.png" width="100" height="172"/>
+</tileset>
