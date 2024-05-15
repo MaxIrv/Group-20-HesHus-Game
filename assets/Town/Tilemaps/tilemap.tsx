04325910ae5224effbcaae856f99c3da51ff322c
SJButla <160864718+SJButla@users.noreply.github.com>
2024-05-15 14:25:42 +0100

    town map updated

---
+++ assets/Town/Tilemaps/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tilemap" tilewidth="24" tileheight="24" tilecount="1160" columns="58">
+ <image source="tilemap.png" width="1396" height="500"/>
+</tileset>
