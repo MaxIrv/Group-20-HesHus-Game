0825cc8c80bb863f0a9b2fa0412dfd6d70abae6b
MaxIrv <moatlands.max@gmail.com>
2024-05-23 08:02:31 +0100

    Update tests

--- tests/src/com/skloch/game/tests/components/DialogueBoxTests.java
+++ tests/src/com/skloch/game/tests/components/DialogueBoxTests.java
@@ -13,25 +13,25 @@ import org.mockito.Mockito;
 
 @RunWith(GdxTestRunner.class)
 public class DialogueBoxTests {
-    DialogueBox dialogueBox;
-    @Mock
-    Skin skin = Mockito.mock(Skin.class);
+  DialogueBox dialogueBox;
+  @Mock
+  Skin skin = Mockito.mock(Skin.class);
 
-    @Before
-    public void setup(){
-        dialogueBox = new DialogueBox(skin);
-    }
+  @Before
+  public void setup() {
+    dialogueBox = new DialogueBox(skin);
+  }
 
-    @Test
-    public void testDialogueBoxConstructor(){
-        Assert.assertEquals(35, dialogueBox.getMaxChars());
-        Assert.assertSame(Window.class, dialogueBox.getDialogueWindow());
-        Assert.assertSame(skin, dialogueBox.getDialogueWindow().getSkin());
+  @Test
+  public void testDialogueBoxConstructor() {
+    Assert.assertEquals(35, dialogueBox.getMaxChars());
+    Assert.assertSame(Window.class, dialogueBox.getDialogueWindow());
+    Assert.assertSame(skin, dialogueBox.getDialogueWindow().getSkin());
 
-        Assert.assertEquals(800, dialogueBox.getDialogueWindow().getWidth());
-        Assert.assertEquals(200, dialogueBox.getDialogueWindow().getHeight());
+    Assert.assertEquals(800, dialogueBox.getDialogueWindow().getWidth());
+    Assert.assertEquals(200, dialogueBox.getDialogueWindow().getHeight());
 
-        Assert.assertSame(Window.class, dialogueBox.getSelectBox());
-    }
+    Assert.assertSame(Window.class, dialogueBox.getSelectBox());
+  }
 
 }
--- tests/src/com/skloch/game/tests/components/GameScreenTests.java
+++ tests/src/com/skloch/game/tests/components/GameScreenTests.java
@@ -1,230 +1,210 @@
 package com.skloch.game.tests.components;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.maps.MapObjects;
-import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.badlogic.gdx.InputMultiplexer;
+import com.skloch.game.GameMap;
 import com.skloch.game.GameObject;
 import com.skloch.game.GameOverScreen;
 import com.skloch.game.GameScreen;
 import com.skloch.game.HustleGame;
-import com.skloch.game.events.EventBus;
-import com.skloch.game.interfaces.*;
-
+import com.skloch.game.interfaces.GameLogicInterface;
+import com.skloch.game.interfaces.GameRendererInterface;
+import com.skloch.game.interfaces.GameUiInterface;
+import com.skloch.game.interfaces.PlayerInterface;
 import com.skloch.game.tests.GdxTestRunner;
-import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 @RunWith(GdxTestRunner.class)
 public class GameScreenTests {
 
-    @Mock
-    private HustleGame mockGame;
+  @Mock
+  private HustleGame mockGame;
 
-    @Mock
-    private IGameLogic mockGameLogic;
+  @Mock
+  private GameLogicInterface mockGameLogic;
 
-    @Mock
-    private IGameRenderer mockGameRenderer;
+  @Mock
+  private GameRendererInterface mockGameRenderer;
 
-    @Mock
-    private IGameUI mockGameUI;
+  @Mock
+  private GameUiInterface mockGameUI;
 
-    @Mock
-    private EventBus mockEventBus;
+  @Mock
+  private GameMap mockGameMap;
 
-    @Mock
-    private IPlayer mockPlayer;
+  @Mock
+  private PlayerInterface mockPlayer;
 
-    private GameScreen gameScreen;
+  private GameScreen gameScreen;
 
-    @Before
-    public void setup() {
+  @Before
+  public void setup() {
 
-        when(mockGameLogic.getPlayer()).thenReturn(mockPlayer);
+    when(mockGameLogic.getPlayer()).thenReturn(mockPlayer);
 
-        gameScreen = new GameScreen(mockGame, 0);
-    }
+    gameScreen = new GameScreen(mockGame, 0);
+  }
 
-    @Test
-    public void testShow() {
-        // Test the show method if needed. Currently, it does nothing.
-        gameScreen.show();
-        // Add assertions or verifications if the show method is expanded.
-    }
+  @Test
+  public void testShow() {
+    // Test the show method if needed. Currently, it does nothing.
+    gameScreen.show();
+    // Add assertions or verifications if the show method is expanded.
+  }
 
-    @Test
-    public void testRender() {
-        // Mocking dependencies that the render method will call
-        float delta = 0.016667f;
+  @Test
+  public void testRender() {
+    // Mocking dependencies that the render method will call
+    float delta = 0.016667f;
 
-        gameScreen.render(delta);
+    gameScreen.render(delta);
 
-        verify(mockGameLogic, times(1)).update(delta);
-        verify(mockGameRenderer, times(1)).render(delta, mockPlayer);
-        verify(mockGameUI, times(1)).render_ui(delta);
-    }
+    verify(mockGameLogic, times(1)).update(delta);
+    verify(mockGameRenderer, times(1)).render(delta, mockPlayer, mockGameMap);
+    verify(mockGameUI, times(1)).renderUi(delta);
+  }
 
-    @Test
-    public void testIsDialogueBoxVisible() {
-        when(mockGameUI.getDialogueBox().isVisible()).thenReturn(true);
+  @Test
+  public void testIsDialogueBoxVisible() {
+    when(mockGameUI.getDialogueBox().isVisible()).thenReturn(true);
 
-        assertTrue(gameScreen.isDialogueBoxVisible());
+    assertTrue(gameScreen.isDialogueBoxVisible());
 
-        when(mockGameUI.getDialogueBox().isVisible()).thenReturn(false);
+    when(mockGameUI.getDialogueBox().isVisible()).thenReturn(false);
 
-        assertFalse(gameScreen.isDialogueBoxVisible());
-    }
+    assertFalse(gameScreen.isDialogueBoxVisible());
+  }
 
-    @Test
-    public void testIsEscapeMenuVisible() {
-        when(mockGameUI.isEscapeMenuVisible()).thenReturn(true);
+  @Test
+  public void testIsEscapeMenuVisible() {
+    when(mockGameUI.isEscapeMenuVisible()).thenReturn(true);
 
-        assertTrue(gameScreen.isEscapeMenuVisible());
+    assertTrue(gameScreen.isEscapeMenuVisible());
 
-        when(mockGameUI.isEscapeMenuVisible()).thenReturn(false);
+    when(mockGameUI.isEscapeMenuVisible()).thenReturn(false);
 
-        assertFalse(gameScreen.isEscapeMenuVisible());
-    }
+    assertFalse(gameScreen.isEscapeMenuVisible());
+  }
 
-    @Test
-    public void testIsPlayerNearObject() {
-        when(mockGameLogic.isPlayerNearObject()).thenReturn(true);
+  @Test
+  public void testIsPlayerNearObject() {
+    when(mockGameLogic.isPlayerNearObject()).thenReturn(true);
 
-        assertTrue(gameScreen.isPlayerNearObject());
+    assertTrue(gameScreen.isPlayerNearObject());
 
-        when(mockGameLogic.isPlayerNearObject()).thenReturn(false);
+    when(mockGameLogic.isPlayerNearObject()).thenReturn(false);
 
-        assertFalse(gameScreen.isPlayerNearObject());
-    }
+    assertFalse(gameScreen.isPlayerNearObject());
+  }
 
-    @Test
-    public void testIsPlayerSleeping() {
-        when(mockGameLogic.isSleeping()).thenReturn(true);
+  @Test
+  public void testIsPlayerSleeping() {
+    when(mockGameLogic.isSleeping()).thenReturn(true);
 
-        assertTrue(gameScreen.isPlayerSleeping());
+    assertTrue(gameScreen.isPlayerSleeping());
 
-        when(mockGameLogic.isSleeping()).thenReturn(false);
+    when(mockGameLogic.isSleeping()).thenReturn(false);
 
-        assertFalse(gameScreen.isPlayerSleeping());
-    }
+    assertFalse(gameScreen.isPlayerSleeping());
+  }
 
-    @Test
-    public void testGetPlayerClosestObject() {
-        GameObject mockGameObject = mock(GameObject.class);
-        when(mockGameLogic.getPlayerClosestObject()).thenReturn(mockGameObject);
+  @Test
+  public void testGetPlayerClosestObject() {
+    GameObject mockGameObject = mock(GameObject.class);
+    when(mockGameLogic.getPlayerClosestObject()).thenReturn(mockGameObject);
 
-        assertEquals(mockGameObject, gameScreen.getPlayerClosestObject());
-    }
+    assertEquals(mockGameObject, gameScreen.getPlayerClosestObject());
+  }
 
-    @Test
-    public void testGetPlayer() {
-        assertEquals(mockPlayer, gameScreen.getPlayer());
-    }
+  @Test
+  public void testGetPlayer() {
+    assertEquals(mockPlayer, gameScreen.getPlayer());
+  }
 
-    @Test
-    public void testResize() {
-        int width = 800;
-        int height = 600;
+  @Test
+  public void testResize() {
+    int width = 800;
+    int height = 600;
 
-        gameScreen.resize(width, height);
+    gameScreen.resize(width, height);
 
-        verify(mockGameUI, times(1)).resize_ui(width, height);
-        verify(mockGameRenderer, times(1)).resize_viewport(width, height);
-    }
+    verify(mockGameUI, times(1)).resizeUi(width, height);
+    verify(mockGameRenderer, times(1)).resizeViewport(width, height);
+  }
 
-    @Test
-    public void testResume() {
-        InputMultiplexer inputMultiplexer = mock(InputMultiplexer.class);
+  @Test
+  public void testResume() {
+    InputMultiplexer inputMultiplexer = mock(InputMultiplexer.class);
 
-        gameScreen.resume();
+    gameScreen.resume();
 
-        verify(inputMultiplexer, times(1)).addProcessor(any());
-        // Further verifications if needed.
-    }
+    verify(inputMultiplexer, times(1)).addProcessor(any());
+    // Further verifications if needed.
+  }
 
-    @Test
-    public void testDispose() {
-        gameScreen.dispose();
+  @Test
+  public void testDispose() {
+    gameScreen.dispose();
 
-        verify(mockGameUI, times(1)).dispose();
-        verify(mockGameRenderer, times(1)).dispose();
-    }
+    verify(mockGameUI, times(1)).dispose();
+    verify(mockGameRenderer, times(1)).dispose();
+  }
 
-    @Test
-    public void testGameOver() {
-        gameScreen.GameOver();
+  @Test
+  public void testGameOver() {
+    gameScreen.gameOver();
 
-        verify(mockGame, times(1)).setScreen(any(GameOverScreen.class));
-    }
+    verify(mockGame, times(1)).setScreen(any(GameOverScreen.class));
+  }
 
-    @Test
-    public void testAddStudyStreakCounter() {
-        gameScreen.setStudyStreakCounter(0);
-        gameScreen.addStudyStreakCounter(5);
+  @Test
+  public void testAddStudyStreakCounter() {
+    gameScreen.setStudyStreakCounter(0);
+    gameScreen.addStudyStreakCounter(5);
 
-        assertEquals(5, gameScreen.getStudyStreakCounter());
-    }
+    assertEquals(5, gameScreen.getStudyStreakCounter());
+  }
 
-    @Test
-    public void testAddBookWormCounter() {
-        gameScreen.setBookWormCounter(0);
-        gameScreen.addBookWormCounter(5);
+  @Test
+  public void testAddBookWormCounter() {
+    gameScreen.setBookWormCounter(0);
+    gameScreen.addBookWormCounter(5);
 
-        assertEquals(5, gameScreen.getBookWormCounter());
-    }
+    assertEquals(5, gameScreen.getBookWormCounter());
+  }
 
-    @Test
-    public void testAddEatStreakCounter() {
-        gameScreen.setEatStreakCounter(0);
-        gameScreen.addEatStreakCounter(5);
+  @Test
+  public void testAddEatStreakCounter() {
+    gameScreen.setEatStreakCounter(0);
+    gameScreen.addEatStreakCounter(5);
 
-        assertEquals(5, gameScreen.getEatStreakCounter());
-    }
+    assertEquals(5, gameScreen.getEatStreakCounter());
+  }
 
-    @Test
-    public void testAddFunStreakCounter() {
-        gameScreen.setFunStreakCounter(0);
-        gameScreen.addFunStreakCounter(5);
+  @Test
+  public void testAddFunStreakCounter() {
+    gameScreen.setFunStreakCounter(0);
+    gameScreen.addFunStreakCounter(5);
 
-        assertEquals(5, gameScreen.getFunStreakCounter());
-    }
+    assertEquals(5, gameScreen.getFunStreakCounter());
+  }
 
-    @Test
-    public void testAddNoSleepCounter() {
-        gameScreen.setNoSleepCounter(0);
-        gameScreen.addNoSleepCounter(5);
+  @Test
+  public void testAddNoSleepCounter() {
+    gameScreen.setNoSleepCounter(0);
+    gameScreen.addNoSleepCounter(5);
 
-        assertEquals(5, gameScreen.getNoSleepCounter());
-    }
+    assertEquals(5, gameScreen.getNoSleepCounter());
+  }
 }
